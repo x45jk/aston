89fdd7a7280cea9d8a1c42ef40df35c95ce30e5c
user <student_802@mail.ru>
2026-08-15 16:03:40 +0300

    create class Product

--- src/main/java/org/example/LessonTwo.java
+++ src/main/java/org/example/LessonTwo.java
@@ -2,5 +2,38 @@ package org.example;
 
 public class LessonTwo {
     public static void main(String[] args) {
+        System.out.println("===== Домашнее задание к Лекции 2.2 =====");
+        // I-е задание
+        System.out.println("--- Задание №1 ---");
+        System.out.println("[Класс 'Product' создан]");
+    }
+}
+
+// I-е задание
+class Product{
+    private String productName;
+    private String manufactureDate;
+    private String manufacturer;
+    private String countryOfOrigin;
+    private double price;
+    private boolean isReserved;
+
+    Product(String productName, String manufactureDate, String manufacturer,
+            String countryOfOrigin, double price, boolean isReserved) {
+        this.productName = productName;
+        this.manufactureDate = manufactureDate;
+        this.manufacturer = manufacturer;
+        this.countryOfOrigin = countryOfOrigin;
+        this.price = price;
+        this.isReserved = isReserved;
+    }
+
+    void printInfoAboutProduct() {
+        System.out.println("Название:                 " + productName);
+        System.out.println("Дата производства:        " + manufactureDate);
+        System.out.println("Производитель:            " + manufacturer);
+        System.out.println("Страна происхождения:     " + countryOfOrigin);
+        System.out.println("Цена:                     " + price);
+        System.out.println("Забронирован покупателем: " + (isReserved ? "Да" : "Нет"));
     }
 }
