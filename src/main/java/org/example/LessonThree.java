9a1fbc9310e1aa5802a1d2c18a15d1082e42f12d
user <student_802@mail.ru>
2026-08-17 22:03:33 +0300

    create branch Lesson_3

---
+++ src/main/java/org/example/LessonThree.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class LessonThree {
+    public static void main(String[] args) {
+        System.out.println("===== Домашнее задание к Лекции 2.4 =====");
+
+        // I-е задание
+        System.out.println("--- Задание №1 ---");
+    }
+}
