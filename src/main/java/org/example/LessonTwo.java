b532a77e013206809a3c4c432ff4d2f16eecf277
user <student_802@mail.ru>
2026-08-20 16:38:22 +0300

    commented on unnecessary

--- src/main/java/org/example/LessonTwo.java
+++ src/main/java/org/example/LessonTwo.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class LessonTwo {
     public static void main(String[] args) {
-        System.out.println("===== Домашнее задание к Лекции 2.3 =====");
+/*        System.out.println("===== Домашнее задание к Лекции 2.3 =====");
 
         // I-е задание
         System.out.println("--- Задание №1 ---");
@@ -20,12 +20,12 @@ public class LessonTwo {
         productsArray[3] = new Product("Clean Code",        "01.08.2008", "Robert C. Martin", "USA",   45.50,   false);
         productsArray[4] = new Product("PlayStation 5",     "12.11.2024", "Sony Interactive", "Japan", 499.00,  true);
         System.out.println("[Массив из 5 товаров заполнен]");
-        /*for (int i = 0; i < productsArray.length; i++) {
+        for (int i = 0; i < productsArray.length; i++) {
             System.out.println("Продукт №" + (i +
                     1) );
             productsArray[i].printInfoAboutProduct();
             System.out.println();
-        }*/
+        }
 
         // III-е задание
         System.out.println("\n--- Задание №3 ---");
@@ -38,6 +38,6 @@ public class LessonTwo {
         park.addAttraction("Автодром",             "ежедневно с 12:00 до 22:00",                       400.0);
         park.addAttraction("Водные горки",         "ежедневно с 10:00 до 21:45",                       350.0);
         System.out.println("[Объект Part заполнен пятью аттракционами]");
-//        park.printAttractions();
+        park.printAttractions();*/
     }
 }
