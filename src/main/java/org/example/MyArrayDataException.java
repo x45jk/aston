b79d903d3e68f8bb894f49918274417f3459c675
user <student_802@mail.ru>
2026-08-23 16:44:35 +0300

    rename branch Lesson_2.5 to Lesson_4

--- src/main/java/org/example/MyArrayDataException.java
+++ src/main/java/org/example/MyArrayDataException.java
@@ -3,6 +3,6 @@ package org.example;
 // unchecked exception
 public class MyArrayDataException extends RuntimeException{
     public MyArrayDataException(int x, int y) {
-        super("There is incorrect data in cell [" + (x+1) + ", " + (y+1) + "]." );
+        super("There is incorrect data in cell [" + (x + 1) + ", " + (y + 1) + "]." );
     }
 }
